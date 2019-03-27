cdae74d4099170f7ba1a7f62306ae20e8bb12c3a
Farrow <U1162369@emea.mrshmc.com>
2019-03-27 15:55:55 +0000

    typos

---
+++ younify/fingerprinting.py
@@ -0,0 +1,8 @@
+import json
+
+"""""Purpose of this is to interface the fingerprinting apis with the rest of the program for a 
+last ditch effort (third pass) at identifying songs correctly, either from file or from url"""
+
+class fingerprinter
+    def __init__(self):
+        print("placeholders")
