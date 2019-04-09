3da3cdb6050a94221580be201cb5c293a7ac4762
Robert Farrow <rob.farrow@hotmail.com>
2019-04-09 15:59:33 +0100

    cleanup commit

--- younify/test/yt_frontend_test.py
+++ younify/test/yt_frontend_test.py
@@ -1,16 +1,8 @@
-#import time
 from younify import yt_frontend
-from younify import youtube_converter
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5 import QtGui, QtWidgets
-import json
-from younify import breeze_resources
-import ctypes
 import unittest
 
 class TestFrameworkMethods(unittest.TestCase):
-    def launch_gui(self):
+    def test_launch_gui(self):
         app = QApplication(["Younify"])
         # trayIcon = QtWidgets.QWinTaskbarButton(QtGui.QIcon(icon), app)
         # trayIcon.show()
