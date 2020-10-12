8d899111dd388e43b166798f5dfd545fdebf1ff4
Rob F <rob.farrow@hotmail.com>
2020-10-12 20:17:26 +0100

    Deployed website, fixed errors in terraformed resources. Added basic lambda deployable.

---
+++ tf/modules/lambda/lambda.tf
@@ -0,0 +1,13 @@
+# ====================================================================
+# TERRAFORM LAMBDAS
+# ====================================================================
+#   This module handles lambda functions
+#  - Ensure all resources have tags
+#  - Ensure everything has a description
+
+//resource "aws_lambda_function" "test" {
+//  function_name = ""
+//  handler = ""
+//  role = ""
+//  runtime = ""
+//}
